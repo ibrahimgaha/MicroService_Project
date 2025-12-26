15e62884929293f4e0d111e5314011853b92832f
briki <brikim647@gmail.com>
2025-12-26 21:47:35 +0100

    configserver

--- user-service/src/main/java/com/example/gestionuser/entity/User.java
+++ user-service/src/main/java/com/example/gestionuser/entity/User.java
@@ -19,4 +19,44 @@ public class User implements Serializable {
     private String prenom;
     private String email;
     private String telephone;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
